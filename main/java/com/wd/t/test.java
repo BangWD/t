b7c846174bbdf481cf922dd897a20876691dff71
BangWD <1539085583@qq.com>
2019-07-17 16:07:39 +0800

    1.0

---
+++ main/java/com/wd/t/test.java
@@ -0,0 +1,9 @@
+package com.wd.t;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("sss");
+        System.out.println("sss");
+        System.out.println("sss");
+    }
+}
